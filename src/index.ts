e2445d29499b879df511bfb132166a5d99c15bb1
Juan Esteban Idarraga <juan.idarraga@globant.com>
2022-10-27 00:51:05 -0500

    section 3: create observables, manage suscriptions and create an observer

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
+import {Observable, Observer} from 'rxjs';
 
+const observer: Observer<any> = {
+    next: value => console.log('next', value),
+    error: error => console.log('error', error),
+    complete: () => console.log('Completed')
+}
 
-console.log('Hola Mundo!');
+const interval$ = new Observable<number>( subscriber => {
+    let count = 0
+    const interval = setInterval(()=>{
+        subscriber.next(count++)
+        console.log(count)
+    }, 1000);
 
+    // setTimeout(() => {
+    //     subscriber.complete() //run complete and return
+    // }, 2000);
 
+    return () => {
+        clearInterval(interval)
+        console.log('interval finalizado')
+    }
+})
 
+const subscription = interval$.subscribe( observer )
 
-
-
-
+setTimeout(()=>{
+    subscription.unsubscribe() // run the return
+}, 4000)
